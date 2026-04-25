5851fcd50e97ace4f647e2d3b21847b9597cef93
Mario Rest <manizaleno_18@hotmail.com>
2026-04-25 18:28:37 -0400

    Revert "adding Chat UI and API route"
    This reverts commit 53a02db88b6da42be0186c94f5b7795c92c3d2d2.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import { Navbar } from "@/components/navbar"
 import { Hero } from "@/components/hero"
 import { Stats } from "@/components/stats"
 import { HealthImpacts, Impacts } from "@/components/impacts"
-import { Chat } from "@/components/chat"
 import { Quote, Footer } from "@/components/quote-footer"
 
 export default function Home() {
@@ -20,7 +19,6 @@ export default function Home() {
           <Stats />
           <HealthImpacts />
           <Impacts />
-          <Chat />
           <Quote />
         </main>
         <Footer />
